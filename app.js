b0298bf1b76f5cbdaeced0025b40e266712eae6c
YaroslavYaroslavtsev <yaroslav.yaroslavtsev@nobitlost.com>
2019-01-19 14:40:21 +0700

    https

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var authenticate = require('./authenticate');
 var config = require('./config');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/userRouter');
+var usersRouter = require('./routes/users');
 
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
@@ -31,6 +31,17 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 
 app.use(cookieParser('12345-67890-09876-54321'));
 
